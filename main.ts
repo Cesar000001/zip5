880430d1abbd93f3fdf6bb089872488650ad1898
Cesar000001 <116392368+Cesar000001@users.noreply.github.com>
2022-11-22 17:52:56 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,41 @@
-let strip: neopixel.Strip = null
-basic.forever(function () {
-    strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB_RGB)
+input.onButtonPressed(Button.A, function () {
+	
+})
+function orange () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Orange))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Orange))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Orange))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Orange))
+}
+function rouge () {
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+}
+function vert () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Yellow))
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.White))
-    strip.setBrightness(63.75)
-    while (true) {
-        strip.show()
-        basic.pause(100)
-        strip.clear()
-    }
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Green))
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P3, 5, NeoPixelMode.RGB)
+strip.setBrightness(100)
+basic.forever(function () {
+    rouge()
+    strip.show()
+    strip.clear()
+    basic.pause(10000)
+    orange()
+    strip.show()
+    strip.clear()
+    basic.pause(1000)
+    vert()
+    strip.show()
+    strip.clear()
+    basic.pause(10000)
 })
